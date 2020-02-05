287c2c9873dd928eece0be7c8b86df9adb3799ab
Victor Tran <victor.n.tran@outlook.com>
2020-02-04 22:21:15 -0500

    correct import statement due to named export

--- index.js
+++ index.js
@@ -1,7 +1,6 @@
-const express = require('express')
-const app = require('./server');
-const port = process.env.PORT || 3000
+const { app } = require("./server");
+const port = process.env.PORT || 3000;
 
 app.listen(port, () => {
   console.log(`App is listening to port ${port}`);
-})
+});
