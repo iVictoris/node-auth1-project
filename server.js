ac1db19e7ab78d20bb71c62cca7728f31044a69e
Victor Tran <victor.n.tran@outlook.com>
2020-02-04 22:22:59 -0500

    set up routes in app

--- server.js
+++ server.js
@@ -4,4 +4,8 @@ const app = express()
 app.use(express.json())
 app.use(express.urlencoded({extended: true}));
 
+app.use('/api/register', registerRoute)
+app.use('/api/login', loginRoute)
+app.use('/api/users', usersRoute)
+
 module.exports.app = app;
