67136e0279ad55bc79aacfcd3418d439ea4e6349
Victor Tran <victor.n.tran@outlook.com>
2020-02-06 16:01:18 -0500

    open up user routes

--- server.js
+++ server.js
@@ -3,13 +3,14 @@ const app = express();
 
 const { router: registerRoute } = require("./router/registerRoute");
 const { router: loginRoute } = require("./router/loginRoute");
-// const { router: usersRoute } = require("./router/userRoute");
+const { router: usersRoute } = require("./router/usersRoute");
+const { restrict } = require("./middleware/restrict");
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 app.use("/api/register", registerRoute);
 app.use("/api/login", loginRoute);
-// app.use("/api/users", usersRoute);
+app.use("/api/users", restrict, usersRoute);
 
 module.exports.app = app;
