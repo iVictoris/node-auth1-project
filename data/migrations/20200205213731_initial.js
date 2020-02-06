09464541500236817fd3b84fc85d28a7c80deff5
Victor Tran <victor.n.tran@outlook.com>
2020-02-05 22:24:00 -0500

    create migration

---
+++ data/migrations/20200205213731_initial.js
@@ -0,0 +1,12 @@
+exports.up = async function(knex) {
+  await knex.schema.createTable("users", table => {
+    table.increments("id");
+    table.string("username").notNullable();
+    table.string("password").notNullable();
+    table.string("email").notNullable();
+  });
+};
+
+exports.down = async function(knex) {
+  await knex.schema.dropTableIfExists("users");
+};
