2b506c60e6add6beb8075e073c0e2061314b4dab
Victor Tran <victor.n.tran@outlook.com>
2020-02-06 14:24:41 -0500

    add login route

--- server.js
+++ server.js
@@ -2,14 +2,14 @@ const express = require("express");
 const app = express();
 
 const { router: registerRoute } = require("./router/registerRoute");
-// const { router: loginRoute } = require("./router/loginRoute");
+const { router: loginRoute } = require("./router/loginRoute");
 // const { router: usersRoute } = require("./router/userRoute");
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 app.use("/api/register", registerRoute);
-// app.use("/api/login", loginRoute);
+app.use("/api/login", loginRoute);
 // app.use("/api/users", usersRoute);
 
 module.exports.app = app;
