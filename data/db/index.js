c7b877e66ffbf2067a6cc81309cad90770e9e471
Victor Tran <victor.n.tran@outlook.com>
2020-02-06 14:25:09 -0500

    create find by id and username helpers and export

--- data/db/index.js
+++ data/db/index.js
@@ -1,9 +1,25 @@
 const db = require("../knex.config");
 
+const findUserById = id => {
+  return db("users")
+    .where({ id })
+    .first();
+};
+
+const findByUsername = username => {
+  return db("users")
+    .where({ username })
+    .first();
+};
+
 const addUser = user => {
-  return db("users").insert(user);
+  return db("users")
+    .insert(user)
+    .then(id => findUserById(id[0]));
 };
 
 module.exports = {
-  addUser
+  addUser,
+  findUserById,
+  findByUsername
 };
