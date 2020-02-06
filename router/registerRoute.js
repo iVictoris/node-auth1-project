998556decd4d29498e639923eafa30baf1023564
Victor Tran <victor.n.tran@outlook.com>
2020-02-05 22:24:34 -0500

    create boilerplate for register route

---
+++ router/registerRoute.js
@@ -0,0 +1,12 @@
+const { Router } = require('express');
+const router = Router();
+
+router
+  .route('/')
+  .post(async (req, res) => {
+    /* POST /api/register */
+    
+  });
+
+
+module.exports.router = router;
