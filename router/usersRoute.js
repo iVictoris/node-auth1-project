300bc162f0fef32ee2f955792ff9d54b4b90c853
Victor Tran <victor.n.tran@outlook.com>
2020-02-06 16:06:51 -0500

    import find function and send users to requests to api

--- router/usersRoute.js
+++ router/usersRoute.js
@@ -1,8 +1,20 @@
 const { Router } = require("express");
 const router = Router();
 
+const { find } = require("../data/db");
+
 router.route("/").get(async (req, res) => {
   /* GET /api/users */
+
+  // if logged in -- will create functionality
+  try {
+    const users = await find();
+    res.status(200).json({ data: users });
+  } catch (e) {
+    res.status(500).json({
+      message: "Unexpected response"
+    });
+  }
 });
 
 module.exports.router = router;
