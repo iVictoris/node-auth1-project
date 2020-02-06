a5f0704e52ce2a9d3c1664136e228c5f90c4f243
Victor Tran <victor.n.tran@outlook.com>
2020-02-05 23:58:09 -0500

    write post path for /api/register with password hashing

--- router/registerRoute.js
+++ router/registerRoute.js
@@ -1,12 +1,18 @@
-const { Router } = require('express');
+const { Router } = require("express");
+const bcrypt = require("bcryptjs");
 const router = Router();
 
-router
-  .route('/')
-  .post(async (req, res) => {
-    /* POST /api/register */
-    
-  });
+const { addUser } = require("../data/db");
 
+router.route("/").post(async (req, res) => {
+  /* POST /api/register */
+  const { username, password, email } = req.body;
+  const salt = bcrypt.genSaltSync(10);
+  const hash = bcrypt.hashSync(password, salt);
+
+  const user = { username, password: hash, email };
+  const dbUser = await addUser(user);
+  res.json(user);
+});
 
 module.exports.router = router;
