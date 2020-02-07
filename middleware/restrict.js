18cde132ff0f474a797fe4425fb293e1c249e356
Victor Tran <victor.n.tran@outlook.com>
2020-02-07 13:33:18 -0500

    change restrict middleware to check for session

--- middleware/restrict.js
+++ middleware/restrict.js
@@ -1,27 +1,14 @@
 const bcrypt = require("bcryptjs");
 const { findByUsername } = require("../data/db");
 
-const restrict = async ({ body: { username, password } }, res, next) => {
-  if (!username || !password) {
-    return res.status(400).json({
-      message: "Invalid credentials supplied or missing"
+const restrict = async (req, res, next) => {
+  console.log(req.session);
+  if (!req.session || !req.session.user) {
+    return res.status(404).json({
+      message: "You shall not pass."
     });
   }
-
-  try {
-    const user = await findByUsername(username);
-    if (bcrypt.compareSync(password, user.password)) {
-      next();
-      return;
-    }
-    res.status(401).json({
-      message: "Invalid credentials, wrong username or password."
-    });
-  } catch (e) {
-    res.status(500).json({
-      message: "Issue in searching for user, please try again later."
-    });
-  }
+  next();
 };
 
 module.exports.restrict = restrict;
